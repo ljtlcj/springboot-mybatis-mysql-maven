df515cb6dc2423ee7b2ed5a2347ce3997ab4c7a0
ljtlcj <1521826348@qq.com>
2019-10-13 17:47:50 +0800

    2

--- springboot+mybatis+mysql/src/main/java/com/example/DemoApplication.java
+++ springboot+mybatis+mysql/src/main/java/com/example/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("test1");
         SpringApplication.run(DemoApplication.class, args);
     }
 
